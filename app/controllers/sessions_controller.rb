16217ea767ae3158e338ce26f701f1774bb7464c
Srija S <srijakumar7@gmail.com>
2019-06-22 21:52:06 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-  
+     @user = User.find_by(name: params[:user][:name])
+     if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "Welcome"
+    else
+      redirect_to signin_path
+    end
   end
 
  def destroy
