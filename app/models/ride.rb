fbaa9bfd81ec0813af3e9848df02498456210e4c
Srija S <srijakumar7@gmail.com>
2019-06-23 19:07:38 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,6 +4,8 @@ class Ride < ActiveRecord::Base
 
   def take_ride
     #byebug
+
+
     if user.tickets < attraction.tickets && user.height < attraction.min_height
       return "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
     end
@@ -13,16 +15,21 @@ class Ride < ActiveRecord::Base
     if user.height < attraction.min_height
       return "Sorry. You are not tall enough to ride the Roller Coaster."
     end
-  end
 
+    @user = User.find_by(name: params[:user][:name])
 
+    params[:user][:happiness] = self.user.happiness + self.attraction.happiness_rating,
+    params[:user][:nausea] = self.user.nausea + self.attraction.nausea_rating,
+    params[:user][:ticket] = self.user.tickets - self.attraction.tickets
 
-    def self.user.update(
-
-      :happiness => self.user.happiness + self.attraction.happiness_rating,
-      :nausea => self.user.nausea + self.attraction.nausea_rating,
-      :ticket => self.user.tickets - self.attraction.tickets
+    @user.update(
+    :happiness => params[:user][:happiness],
+    :nausea => params[:user][:nausea],
+    :ticket => params[:user][:ticket]
     )
+
+
+  end
 
 
 end
