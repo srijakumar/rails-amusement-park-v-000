654cfda6490aa9b17fbe1e8dd303a411c21e7960
Srija S <srijakumar7@gmail.com>
2019-06-29 14:43:36 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     #validates :admin, :presence => true
 
     def mood
-      if self.happiness < self.nausea
+      if (self.happiness < self.nausea)
         return 'sad'
       else
         return 'happy'
